1e8eaf4b30f2227a39d36055f5ac7172af49504f
191012 <kimhj191012@naver.com>
2024-05-03 16:04:29 +0900

    Enemy move to player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject enemyObject;
+    private Transform player;
+    
+    void Update() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        float distanceVector = Vector3.Distance(transform.position, player.position);
+
+        transform.position = Vector3.MoveTowards(transform.position, player.position, 1.5f * Time.deltaTime);
+    }
 }
