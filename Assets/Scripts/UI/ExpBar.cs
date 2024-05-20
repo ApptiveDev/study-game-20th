0a9a49336309c9af31a474fdbb55d03a844014ad
mandu <jys0972@gmail.com>
2024-05-20 19:51:09 +0900

    remove Debug print

--- Assets/Scripts/UI/ExpBar.cs
+++ Assets/Scripts/UI/ExpBar.cs
@@ -24,20 +24,12 @@ public class ExpBar : MonoBehaviour
 
     void LevelUp()
     {
-        print("Level Up");
         weaponManager.ShowWeaponSelectPage();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         weaponManager = GameManager.Instance.getWeaponManager();
         slider = gameObject.GetComponent<Slider>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
