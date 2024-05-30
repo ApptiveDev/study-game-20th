bb4caf3b19ee5f8b1183f22921b2b933ea421707
mandu <jys0972@gmail.com>
2024-05-30 13:37:43 +0900

    Add Game Clear
    Add Game Clear, when boss is killed

--- Assets/Scripts/GameScene/EndOfGameController.cs
+++ Assets/Scripts/GameScene/EndOfGameController.cs
@@ -2,27 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
-public class GameOverController: MonoBehaviour
+public class EndOfGameController: MonoBehaviour
 {
 
     GameManager gameManager;
-
+    TMP_Text text;
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameManager.Instance;
+        text = gameObject.GetComponent<TMP_Text>();
         gameObject.SetActive(false);
     }
 
-    public void PlayerDead()
+    public void GameClearOrOver(bool isGameClear)
     {
         gameObject.SetActive(true);
         Time.timeScale = 0.1f;
-        StartCoroutine(GameOvered());
+        if (isGameClear)
+        {
+            text.text = "GameClear";
+        } else
+        {
+            text.text = "GameOver";
+        }
+        StartCoroutine(GameEnd());
     }
 
-    IEnumerator GameOvered()
+    IEnumerator GameEnd()
     {
         yield return new WaitForSecondsRealtime(1.0f);
 
